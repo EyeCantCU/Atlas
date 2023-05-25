e219a0b8b6b34ab8307ab9014824f1d0d14aafbb
EyeCantCU <EyeCantCU@users.noreply.github.com>
2023-05-24 20:36:01 -0600

    fix: Purge post-build scripts - RTFM

--- scripts/post/get-upnote.sh
+++
@@ -1,3 +0,0 @@
-set -oue pipefail
-wget https://upnote-release.s3.us-west-2.amazonaws.com/UpNote.AppImage -P ~/Desktop
-chmod +x ~/Desktop/UpNote.AppImage
--- scripts/post/setup.sh
+++
@@ -1,2 +0,0 @@
-set -oue pipefail
-just setup-gaming
