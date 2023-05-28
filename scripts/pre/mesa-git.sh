5861a5542753716291944e2e659aadff5387a4a9
EyeCantCU <EyeCantCU@users.noreply.github.com>
2023-05-28 08:06:39 -0600

    fix: Make sure mesa git is actually pulled

--- scripts/pre/mesa-git.sh
+++ scripts/pre/mesa-git.sh
@@ -1,5 +1,5 @@
 # Mesa from git
 rpm-ostree override remove mesa-va-drivers-freeworld
 rpm-ostree override --experimental replace mesa-libglapi mesa-libxatracker mesa-dri-drivers mesa-libgbm mesa-libEGL mesa-libGL \
-        mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers --from repo=mesa-git
+        mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers --from repo=xxmitsu-mesa-git-fedora-38
 rpm-ostree install mesa-va-drivers
