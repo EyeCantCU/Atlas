fcf4949480ca7bc06fe814aa591dd607bcf9e57a
RJ Trujillo <eyecantcu@pm.me>
2023-06-05 17:33:00 -0600

    feat: Provide flatpaks with access to icons
    Namely to address cursor inconsistencies

--- usr/share/ublue-os/firstboot/launcher/autostart.sh
+++ usr/share/ublue-os/firstboot/launcher/autostart.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+# Give flatpaks access to icons
+flatpak --user override --filesystem=/home/$USER/.icons/:ro
+flatpak --user override --filesystem=/usr/share/icons/:ro
+
 # Simply launches the "yafti" GUI with the uBlue image's configuration.
 /usr/bin/yafti /usr/share/ublue-os/firstboot/yafti.yml
