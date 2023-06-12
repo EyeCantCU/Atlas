40c1376fa74d3a849eb87620810e28c6b86576b0
RJ Trujillo <eyecantcu@pm.me>
2023-06-11 22:45:58 -0600

    fix: Apply hosts in /usr as well

--- scripts/post/hosts.sh
+++ scripts/post/hosts.sh
@@ -1,3 +1,4 @@
 set -oue pipefail
 
+wget -q https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O /usr/etc/hosts
 wget -q https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O /etc/hosts
