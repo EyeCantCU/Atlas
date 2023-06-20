87cc67a0adb9880c3898f488d2cc52b26d745189
RJ Trujillo <eyecantcu@pm.me>
2023-06-19 19:22:46 -0600

    fix: Remove mesa-git

--- scripts/pre/mesa-git.sh
+++
@@ -1,7 +0,0 @@
-set -oue pipefail
-
-# Mesa from git
-rpm-ostree override remove mesa-va-drivers-freeworld
-rpm-ostree override --experimental replace mesa-libglapi mesa-libxatracker mesa-dri-drivers mesa-libgbm mesa-libEGL mesa-libGL \
-        mesa-filesystem mesa-vdpau-drivers mesa-vulkan-drivers --from repo=copr:copr.fedorainfracloud.org:xxmitsu:mesa-git
-rpm-ostree install mesa-va-drivers
