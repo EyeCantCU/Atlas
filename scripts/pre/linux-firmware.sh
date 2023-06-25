9ad78f58c8528928b3dbe07bf019695ba61cb8c7
RJ Trujillo <eyecantcu@pm.me>
2023-06-24 19:49:25 -0600

    fix: Drop git linux firmware
    Causing quirky behavior with my controller

--- scripts/pre/linux-firmware.sh
+++
@@ -1,6 +0,0 @@
-set -oue pipefail
-
-# Latest Linux firmware
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git /tmp/linux-firmware
-rm -rf /lib/firmware/*
-mv /tmp/linux-firmware/* /lib/firmware/
