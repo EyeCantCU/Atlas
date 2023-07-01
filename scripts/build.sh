35e8ab5b2a4fa933d331c5ba60d0ffc6427489c5
RJ Trujillo <eyecantcu@pm.me>
2023-07-01 09:28:25 -0600

    chore(yafti): Pull configuration out of the build process
    Yafti should be shipped and enabled regardless

--- scripts/build.sh
+++ scripts/build.sh
@@ -69,41 +69,5 @@ if [[ ${#remove_rpms[@]} -gt 0 ]]; then
     echo "---"
 fi
 
-# Toggle yafti, which provides the "first boot" experience, https://github.com/ublue-os/yafti.
-FIRSTBOOT_DATA="/usr/share/ublue-os/firstboot"
-FIRSTBOOT_LINK="/usr/etc/profile.d/ublue-firstboot.sh"
-if [[ "${YAFTI_ENABLED}" == "true" ]]; then
-    echo "-- firstboot: Installing and enabling \"yafti\" --"
-    pip install --prefix=/usr yafti
-    # Create symlink to our profile script, which creates the per-user "autorun yafti" links.
-    mkdir -p "$(dirname "${FIRSTBOOT_LINK}")"
-    ln -s "${FIRSTBOOT_DATA}/launcher/login-profile.sh" "${FIRSTBOOT_LINK}"
-else
-    echo "-- firstboot: Removing all \"firstboot\" components --"
-    # Removes the script symlink that creates the per-user autostart symlinks.
-    # We must forcibly remove this here, in case it was added by an upstream image.
-    rm -f "${FIRSTBOOT_LINK}"
-    # Remove all of the launcher-scripts and yafti config, to de-clutter image and
-    # ensure it can't run by accident due to lingering symlinks or upstream image.
-    rm -rf "${FIRSTBOOT_DATA}"
-fi
-
-# Add a new yafti "package group" called Custom, for the packages defined in recipe.yml.
-# Only adds the package group if yafti is enabled and Flatpaks are defined in the recipe.
-if [[ "${YAFTI_ENABLED}" == "true" ]]; then
-    YAFTI_FILE="${FIRSTBOOT_DATA}/yafti.yml"
-    get_yaml_array flatpaks '.firstboot.flatpaks[]'
-    if [[ ${#flatpaks[@]} -gt 0 ]]; then
-        echo "-- yafti: Adding Flatpaks defined in recipe.yml --"
-        yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks suggested by the image maintainer."' "${YAFTI_FILE}"
-        yq -i '.screens.applications.values.groups.Custom.default = true' "${YAFTI_FILE}"
-        for pkg in "${flatpaks[@]}"; do
-            echo "Adding to yafti: ${pkg}"
-            yq -i ".screens.applications.values.groups.Custom.packages += [{\"${pkg}\": \"${pkg}\"}]" "${YAFTI_FILE}"
-        done
-        echo "---"
-    fi
-fi
-
 # Run "post" scripts.
 run_scripts "post"
