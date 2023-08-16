d8390dc5930699b037467ccc2f6bf5addd1b7d3b
RJ Trujillo <eyecantcu@pm.me>
2023-08-16 11:29:19 -0600

    feat: Add just scripts back

---
+++ usr/share/ublue-os/just/ublue-os-just.sh
@@ -0,0 +1,12 @@
+alias just="just --unstable"
+
+# Add uBlue's justfiles to users with home directories which lack a justfile.
+
+if [ ! -z "$HOME" ] && [ -d "$HOME" ] && [ ! -f "${HOME}/.justfile" ]; then
+  cat > "${HOME}/.justfile" << EOF
+!include /usr/share/ublue-os/just/main.just
+!include /usr/share/ublue-os/just/nvidia.just
+!include /usr/share/ublue-os/just/custom.just
+!include /usr/share/ublue-os/just/atlas.just
+EOF
+fi
