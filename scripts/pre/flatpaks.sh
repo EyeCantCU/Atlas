1ba22cd3c1b495e737c6825d36479e251849bd62
RJ Trujillo <eyecantcu@pm.me>
2023-08-19 19:00:30 -0600

    feat(flatpaks): Include VLC

--- scripts/pre/flatpaks.sh
+++ scripts/pre/flatpaks.sh
@@ -9,3 +9,4 @@ sed -i 's/org.mozilla.firefox\n//g' ${INSTALL}
 echo com.brave.Browser >> ${INSTALL}
 echo com.raggesilver.BlackBox >> ${INSTALL}
 echo org.qbittorrent.qBittorrent >> ${INSTALL}
+echo org.videolan.VLC >> ${INSTALL}
