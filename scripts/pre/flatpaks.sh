26ed4fe0ecc6fe42257f47fa514f92597e5d776a
RJ Trujillo <eyecantcu@pm.me>
2023-08-19 19:01:59 -0600

    feat(flatpaks): Include Easy Effects

--- scripts/pre/flatpaks.sh
+++ scripts/pre/flatpaks.sh
@@ -7,6 +7,7 @@ sed -i 's/org.mozilla.firefox\n//g' ${INSTALL}
 
 # Install
 echo com.brave.Browser >> ${INSTALL}
+echo com.github.wwmm.easyeffects >> ${INSTALL}
 echo com.raggesilver.BlackBox >> ${INSTALL}
 echo org.qbittorrent.qBittorrent >> ${INSTALL}
 echo org.videolan.VLC >> ${INSTALL}
