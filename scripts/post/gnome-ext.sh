de725257013634794680a6d5552b96cef27704f1
RJ Trujillo <eyecantcu@pm.me>
2023-08-19 22:04:56 -0600

    feat: Add and enable Pano clipboard extension

--- scripts/post/gnome-ext.sh
+++ scripts/post/gnome-ext.sh
@@ -2,8 +2,16 @@ set -oue pipefail
 
 mkdir -p /usr/share/gnome-shell/extensions
 
+# Pano
+wget $(curl -s https://api.github.com/repos/oae/gnome-shell-pano/releases/latest | \
+  jq -r ".assets[] | select(.name | test(\"pano@elhan.io.zip\")) | .browser_download_url") \
+  -P /tmp
+unzip /tmp/pano@elhan.io.zip -d /usr/share/gnome-shell/extensions/pano@elhan.io
+rm -f /tmp/pano@elhan.io.zip
+
 # Rounded Window Corners
 wget $(curl -s https://api.github.com/repos/yilozt/rounded-window-corners/releases/latest | \
   jq -r ".assets[] | select(.name | test(\"rounded-window-corners@yilozt.shell-extension.zip\")) | .browser_download_url") \
   -P /tmp
 unzip /tmp/rounded-window-corners@yilozt.shell-extension.zip -d /usr/share/gnome-shell/extensions/rounded-window-corners@yilozt
+rm -f /tmp/rounded-window-corners@yilozt.shell-extension.zip
