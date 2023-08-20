bcc89350ed13a0a76c5c372f6c2a43261c18dea0
RJ Trujillo <eyecantcu@pm.me>
2023-08-19 20:16:49 -0600

    chore: Remove OGUI

--- scripts/post/opengamepadui.sh
+++
@@ -1,7 +0,0 @@
-set -oue pipefail
-
-cd /tmp
-lastversion --assets --filter opengamepadui.tar.gz download https://github.com/ShadowBlip/OpenGamepadUI
-tar xvfz opengamepadui.tar.gz
-cd opengamepadui
-sudo make install PREFIX=/usr
