1d9f907eaf4c1a0a13bd170869568caaeffee825
RJ Trujillo <eyecantcu@pm.me>
2023-08-19 19:04:35 -0600

    feat(flatpaks): Bundle ProtonVPN and ProtonMail bridge

--- scripts/pre/flatpaks.sh
+++ scripts/pre/flatpaks.sh
@@ -6,8 +6,10 @@ INSTALL='/etc/flatpak/install'
 sed -i 's/org.mozilla.firefox\n//g' ${INSTALL}
 
 # Install
+echo ch.protonmail.protonmail-bridge >> ${INSTALL}
 echo com.brave.Browser >> ${INSTALL}
 echo com.github.wwmm.easyeffects >> ${INSTALL}
+echo com.protonvpn.www >> ${INSTALL}
 echo com.raggesilver.BlackBox >> ${INSTALL}
 echo org.qbittorrent.qBittorrent >> ${INSTALL}
 echo org.videolan.VLC >> ${INSTALL}
