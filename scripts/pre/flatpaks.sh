66b6d59958e48533ac7d19d21ac6df4adbe4435a
RJ Trujillo <eyecantcu@pm.me>
2023-08-19 18:59:36 -0600

    feat(flatpaks): Include qBittorrent

--- scripts/pre/flatpaks.sh
+++ scripts/pre/flatpaks.sh
@@ -8,3 +8,4 @@ sed -i 's/org.mozilla.firefox\n//g' ${INSTALL}
 # Install
 echo com.brave.Browser >> ${INSTALL}
 echo com.raggesilver.BlackBox >> ${INSTALL}
+echo org.qbittorrent.qBittorrent >> ${INSTALL}
