2ce55da9a0dbeda9cb64e180075c622eb30cfd07
RJ Trujillo <eyecantcu@pm.me>
2023-08-20 10:28:13 -0600

    feat: Add and enable extension that hides the Universal Access button
    Purely cosmetic, not recommended

--- scripts/post/gnome-ext.sh
+++ scripts/post/gnome-ext.sh
@@ -1,17 +1,23 @@
 set -oue pipefail
 
-mkdir -p /usr/share/gnome-shell/extensions
+EXTENSIONS='/usr/share/gnome-shell/extensions'
+
+mkdir -p ${EXTENSIONS}
 
 # Pano
 wget $(curl -s https://api.github.com/repos/oae/gnome-shell-pano/releases/latest | \
   jq -r ".assets[] | select(.name | test(\"pano@elhan.io.zip\")) | .browser_download_url") \
   -P /tmp
-unzip /tmp/pano@elhan.io.zip -d /usr/share/gnome-shell/extensions/pano@elhan.io
+unzip /tmp/pano@elhan.io.zip -d ${EXTENSIONS}/pano@elhan.io
 rm -f /tmp/pano@elhan.io.zip
 
 # Rounded Window Corners
 wget $(curl -s https://api.github.com/repos/yilozt/rounded-window-corners/releases/latest | \
   jq -r ".assets[] | select(.name | test(\"rounded-window-corners@yilozt.shell-extension.zip\")) | .browser_download_url") \
   -P /tmp
-unzip /tmp/rounded-window-corners@yilozt.shell-extension.zip -d /usr/share/gnome-shell/extensions/rounded-window-corners@yilozt
+unzip /tmp/rounded-window-corners@yilozt.shell-extension.zip -d ${EXTENSIONS}/rounded-window-corners@yilozt
 rm -f /tmp/rounded-window-corners@yilozt.shell-extension.zip
+
+# Hide Universal Access
+git clone https://github.com/akiirui/hide-universal-access ${EXTENSIONS}/hide-universal-access@akiirui.github.io --single-branch --no-tags --depth 1
+rm -rf ${EXTENSIONS}/hide-universal-access@akiirui.github.io/.git
