6ed7dab4be14fa488a6ccc94b7c97bf02a6d9220
RJ Trujillo <eyecantcu@pm.me>
2023-08-22 20:47:19 -0600

    fix(tabby): Move to just script

--- scripts/post/tabby.sh
+++
@@ -1,8 +0,0 @@
-set -oue pipefail
-
-# Tabby
-cd /tmp
-wget $(curl -s https://api.github.com/repos/Eugeny/tabby/releases/latest | \
-  jq -r ".assets[] | select(.name | test(\"linux-x64.rpm\")) | .browser_download_url")
-rpm-ostree install *linux-x64.rpm
-rm -f *linux-x64.rpm
