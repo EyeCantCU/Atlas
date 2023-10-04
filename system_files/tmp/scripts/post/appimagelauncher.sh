c7011e82c88429e9036320609725dc21d4385d21
RJ Trujillo <eyecantcu@pm.me>
2023-10-03 22:28:02 -0600

    chore: Drop AppImageLauncher

--- system_files/tmp/scripts/post/appimagelauncher.sh
+++
@@ -1,5 +0,0 @@
-set -oue pipefail
-
-cd /tmp
-wget -q https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
-rpm-ostree install appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
