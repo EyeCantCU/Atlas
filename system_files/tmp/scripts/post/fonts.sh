b856ad0fec8ccb3adbed642abb16dc7f7819161a
RJ Trujillo <eyecantcu@pm.me>
2023-10-11 16:59:44 -0600

    fix(fonts): Correct path for Intel One Mono

--- system_files/tmp/scripts/post/fonts.sh
+++ system_files/tmp/scripts/post/fonts.sh
@@ -2,7 +2,7 @@ set -oue pipefail
 
 wget -O /tmp/intelmono.zip 'https://github.com/intel/intel-one-mono/releases/latest/download/ttf.zip'
 unzip /tmp/intelmono.zip -d "/tmp/intelmono"
-mv /tmp/intelmono/ttf "$FILES_ROOT/usr/share/fonts/intel-one-mono"
+mv /tmp/intelmono/ttf "/usr/share/fonts/intel-one-mono"
 
 curl -sL $(curl -s https://api.github.com/repos/rsms/inter/releases | jq -r '.[0].assets[0].browser_download_url') -o /tmp/inter.zip
 mkdir -p /tmp/inter /usr/share/fonts/inter
