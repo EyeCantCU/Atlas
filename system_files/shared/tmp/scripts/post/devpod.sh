6d295eb19f034010c8adb38850f9ede38877e4be
RJ Trujillo <eyecantcu@pm.me>
2023-11-02 08:46:21 -0600

    fix(devpod): Drop DevPod RPM, keep CLI
    RPM is no longer maintained

--- system_files/shared/tmp/scripts/post/devpod.sh
+++ system_files/shared/tmp/scripts/post/devpod.sh
@@ -1,7 +1,6 @@
 set -oue pipefail
 
-# Install DevPod
-wget -q https://github.com/loft-sh/devpod/releases/latest/download/DevPod_linux_x86_64.rpm -O /tmp/devpod.rpm && \
-  rpm-ostree install /tmp/devpod.rpm && \
-  wget https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64 -O /tmp/devpod && \
-  install -c -m 0755 /tmp/devpod /usr/bin
+# Install DevPod CLI
+wget https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64 -O /tmp/devpod
+install -c -m 0755 /tmp/devpod /usr/bin
+rm -rf /tmp/devpod
