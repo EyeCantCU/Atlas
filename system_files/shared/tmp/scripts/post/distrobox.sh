70505a2039b9edbfbfcdcd8a8f68b55ec0b581f8
RJ Trujillo <eyecantcu@pm.me>
2023-11-06 12:23:33 -0700

    fix(distrobox-git): Return to /, not /root

--- system_files/shared/tmp/scripts/post/distrobox.sh
+++ system_files/shared/tmp/scripts/post/distrobox.sh
@@ -1,3 +1,9 @@
 set -oue pipefail
 
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next --prefix /usr
+cd /tmp
+wget -q https://raw.githubusercontent.com/89luca89/distrobox/main/install -O distrobox-install.sh
+sed -zi 's/cd\n/cd \/\n/g' distrobox-install.sh
+chmod +x distrobox-install.sh
+./distrobox-install.sh --next --prefix /usr
+rm -f distrobox-install.sh
+cd /
