38477cbc5d4803781357d80d76f7d13710b83bc6
RJ Trujillo <eyecantcu@pm.me>
2023-11-06 11:42:34 -0700

    fix(distrobox-git): Install from last commit

--- system_files/shared/tmp/scripts/post/distrobox.sh
+++ system_files/shared/tmp/scripts/post/distrobox.sh
@@ -1,3 +1,3 @@
 set -oue pipefail
 
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next
