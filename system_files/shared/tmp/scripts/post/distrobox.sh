1935b78d9b68ced9fa1655d6d92c943b4bd5adea
RJ Trujillo <eyecantcu@pm.me>
2023-11-06 11:43:26 -0700

    fix(distrobox-git): Install to /usr

--- system_files/shared/tmp/scripts/post/distrobox.sh
+++ system_files/shared/tmp/scripts/post/distrobox.sh
@@ -1,3 +1,3 @@
 set -oue pipefail
 
-curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next --prefix /usr
