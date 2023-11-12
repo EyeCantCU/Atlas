d5ca87634c5c1aae6f2288ab214d96d0cd55020c
RJ Trujillo <eyecantcu@pm.me>
2023-11-12 14:43:51 -0700

    fix(flatpaks): Handle remove list in script

--- system_files/shared/tmp/scripts/pre/flatpaks.sh
+++ system_files/shared/tmp/scripts/pre/flatpaks.sh
@@ -1,6 +1,7 @@
 set -oue pipefail
 
 INSTALL='/usr/share/ublue-os/bazzite/flatpak/install'
+REMOVE='/usr/share/ublue-os/bazzite/flatpak/remove'
 
 # Install
 echo ch.protonmail.protonmail-bridge >> ${INSTALL}
@@ -19,4 +20,31 @@ echo org.videolan.VLC >> ${INSTALL}
 if [[ "${BASE_IMAGE_NAME}" = "silverblue" ]]; then
   echo com.raggesilver.BlackBox >> ${INSTALL}
   echo org.qbittorrent.qBittorrent >> ${INSTALL}
+fi
+
+# Remove
+if [[ "${BASE_IMAGE_NAME}" = "kinoite" ]]; then
+  echo com.github.GradienceTeam.Gradience >> ${REMOVE}
+  echo com.mattjakeman.ExtensionManager >> ${REMOVE}
+  echo io.missioncenter.MissionCenter >> ${REMOVE}
+  echo com.vysp3r.ProtonPlus >> ${REMOVE}
+  echo org.gnome.Calculator >> ${REMOVE}
+  echo org.gnome.Calendar >> ${REMOVE}
+  echo org.gnome.Characters >> ${REMOVE}
+  echo org.gnome.Contacts >> ${REMOVE}
+  echo org.gnome.Evince >> ${REMOVE}
+  echo org.gnome.Logs >> ${REMOVE}
+  echo org.gnome.Loupe >> ${REMOVE}
+  echo org.gnome.Maps >> ${REMOVE}
+  echo org.gnome.NautilusPreviewer >> ${REMOVE}
+  echo org.gnome.TextEditor >> ${REMOVE}
+  echo org.gnome.Weather >> ${REMOVE}
+  echo org.gnome.baobab >> ${REMOVE}
+  echo org.gnome.Totem >> ${REMOVE}
+  echo org.gnome.clocks >> ${REMOVE}
+  echo org.gnome.font-viewer >> ${REMOVE}
+  echo org.gtk.Gtk3theme.adw-gtk3 >> ${REMOVE}
+  echo org.gtk.Gtk3theme.adw-gtk3-dark >> ${REMOVE}
+elif [[ "${BASE_IMAGE_NAME}" = "silverblue" ]]; then
+  echo net.davidotek.pupgui2 >> ${REMOVE}
 fi
