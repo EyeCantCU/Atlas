92893fc896c0daa5ae61664fbf486c6a48e5cc8b
RJ Trujillo <eyecantcu@pm.me>
2023-11-12 13:36:31 -0700

    fix(protonvpn): Turn off gpgcheck

--- system_files/shared/tmp/scripts/pre/protonvpn.sh
+++ system_files/shared/tmp/scripts/pre/protonvpn.sh
@@ -9,6 +9,6 @@ echo '[protonvpn-fedora-stable]
 name = ProtonVPN Fedora Stable repository
 baseurl = https://repo.protonvpn.com/fedora-$releasever-stable
 enabled = 1
-gpgcheck = 1
-repo_gpgcheck=1
+gpgcheck = 0
+repo_gpgcheck=0
 gpgkey = https://repo.protonvpn.com/fedora-$releasever-stable/public_key.asc' | sudo tee /etc/yum.repos.d/protonvpn.repo
