a4350ff90e6685f55a85c16e9129fec7f4c8d7bb
RJ Trujillo <eyecantcu@pm.me>
2023-11-12 12:49:19 -0700

    feat: Add and enable Proton VPN repo

---
+++ system_files/shared/tmp/scripts/pre/protonvpn.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+# Setup Proton VPN repo
+mkdir -p /etc/yum.repos.d
+
+cat > /etc/yum.repos.d/protonvpn.repo <<EOF
+#
+# ProtonVPN stable release
+#
+[protonvpn-fedora-stable]
+name = ProtonVPN Fedora Stable repository
+baseurl = https://repo.protonvpn.com/fedora-$releasever-stable
+enabled = 1
+gpgcheck = 1
+repo_gpgcheck=1
+gpgkey = https://repo.protonvpn.com/fedora-$releasever-stable/public_key.asc
+EOF
