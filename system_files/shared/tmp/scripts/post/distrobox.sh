d2b06e1bf11b3363cdfd7a2c050519b87f64befa
RJ Trujillo <eyecantcu@pm.me>
2024-01-05 10:34:30 -0700

    chore(scripts): Drop distrobox git

--- system_files/shared/tmp/scripts/post/distrobox.sh
+++
@@ -1,9 +0,0 @@
-set -oue pipefail
-
-cd /tmp
-wget -q https://raw.githubusercontent.com/89luca89/distrobox/main/install -O distrobox-install.sh
-sed -zi 's/cd\n/cd \/\n/g' distrobox-install.sh
-chmod +x distrobox-install.sh
-./distrobox-install.sh --next --prefix /usr
-rm -f distrobox-install.sh
-cd /
