85ad61fc8fddf2f4af26ab42bd5454e8513f4db1
RJ Trujillo <eyecantcu@pm.me>
2024-01-05 10:32:47 -0700

    chore(scripts): Drop DevPod script (using RPM now)

--- system_files/shared/tmp/scripts/post/devpod.sh
+++
@@ -1,6 +0,0 @@
-set -oue pipefail
-
-# Install DevPod CLI
-wget https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64 -O /tmp/devpod
-install -c -m 0755 /tmp/devpod /usr/bin
-rm -rf /tmp/devpod
