b00a83df2255cf3ddfb44015401390cf5fbecf74
RJ Trujillo <eyecantcu@pm.me>
2024-01-05 11:57:02 -0700

    chore: Use Pano from ublue-os/staging

--- system_files/shared/tmp/scripts/post/gnome-ext.sh
+++ system_files/shared/tmp/scripts/post/gnome-ext.sh
@@ -4,13 +4,6 @@ EXTENSIONS='/usr/share/gnome-shell/extensions'
 
 mkdir -p ${EXTENSIONS}
 
-# Pano
-wget $(curl -s https://api.github.com/repos/oae/gnome-shell-pano/releases/latest | \
-  jq -r ".assets[] | select(.name | test(\"pano@elhan.io.zip\")) | .browser_download_url") \
-  -P /tmp
-unzip /tmp/pano@elhan.io.zip -d ${EXTENSIONS}/pano@elhan.io
-rm -f /tmp/pano@elhan.io.zip
-
 # Rounded Window Corners
 wget $(curl -s https://api.github.com/repos/yilozt/rounded-window-corners/releases/latest | \
   jq -r ".assets[] | select(.name | test(\"rounded-window-corners@yilozt.shell-extension.zip\")) | .browser_download_url") \
